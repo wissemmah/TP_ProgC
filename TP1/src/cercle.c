9d86180f04f84e29ef4501e11ab561f95978bd07
wissemmah <wissemmahmoudi308@gmail.com>
2025-12-01 14:33:44 +0100

    Update cercle.c

--- TP1/src/cercle.c
+++ TP1/src/cercle.c
@@ -2,9 +2,12 @@
 #include <string.h>
 
 int main() {
-double pi = 3.1416;
-double r = 6;  
-    printf("L'air du cercle = %f", pi*(r*r));
-   printf("Le périmètre du cercle = %f", pi*(r+r));
+    double pi = 3.1416;
+    double r = 6;
+
+    printf("L'aire du cercle = %.4f\n", pi * (r * r));
+    printf("Le perimetre du cercle = %.4f\n", 2 * pi * r);
+
+    return 0;
 }
 
